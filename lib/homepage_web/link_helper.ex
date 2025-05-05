5336e6f6e2b6c777a449120285c556377877d6c0
Paul Sabou <paul.sabou@gmail.com>
2025-05-05 22:07:32 +0200

    updated blogs thumbnails

--- lib/homepage_web/link_helper.ex
+++ lib/homepage_web/link_helper.ex
@@ -1,15 +1,17 @@
 defmodule HomepageWeb.LinkHelper do
   use HomepageWeb, :verified_routes
 
+  alias Homepage.Deployment
+
   def image_path(image) do
     Path.join("/images", image)
   end
 
   def blog_post_thumbnail_path(post) do
-    ~p"/images/posts/#{post.date.year}/#{post.thumbnail}"
+    Deployment.server_url(~p"/images/posts/#{post.date.year}/#{post.thumbnail}")
   end
 
   def blog_post_show(post) do
-    ~p"/blog/#{post.id}"
+    Deployment.server_url(~p"/blog/#{post.id}")
   end
 end
